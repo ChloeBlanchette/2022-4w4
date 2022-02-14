efe1d1ce01c8dc127b6c5436daa51c5fcd1053d8
Chloe Blanchette <chloe.blanchette@outlook.com>
2022-02-14 11:03:45 -0500

    Semaine 4 cours 1

--- footer.php
+++ footer.php
@@ -1,8 +1,13 @@
 <footer class="site__footer">
     <section>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam rerum quaerat repudiandae enim ullam minima, sit perspiciatis fugiat necessitatibus cum voluptate neque blanditiis facere? Aperiam vero excepturi modi a unde!</p>
-        <?php wp_nav_menu(array("menu"=>"simple",
+        <?php
+            $icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#ddd"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>';
+            wp_nav_menu(array("menu"=>"simple",
                                 "container"=>"nav",
-                                "container_class"=>"site__footer__nav")); ?>
+                                "menu_class" => "footer__menu",
+                                "container_class"=>"site__footer__nav",
+                                "link_before" => $icone)); 
+        ?>
     </section>
 </footer>
