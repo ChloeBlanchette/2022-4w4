00f3b4dc30a05ec3e7a6518577e9df40e8bf4ce4
Chloé Blanchette <chloe.blanchette@outlook.com>
2022-03-18 15:47:47 -0400

    s8 c2 Chloé Blanchette Création des trois fichier pour les widgets de la sections colonne du footer

--- sidebar-footer_colonne_1.php
+++ sidebar-footer_colonne_1.php
@@ -1,3 +1,4 @@
+<!-- Création des trois fichier pour les widgets de la sections colonne du footer -->
 <div id="sidebar-footer_colonne_1" class="sidebar">
     <?php dynamic_sidebar('footer_colonne_1'); ?>
 </div>
