6a21f4c11180f3adfc656585cea985de040a7372
Chloé Blanchette <chloe.blanchette@outlook.com>
2022-03-18 15:46:56 -0400

    s8 c2 Chloé Blanchette Création des trois fichier pour les widgets de la sections ligne du footer

--- sidebar-footer_ligne_1.php
+++ sidebar-footer_ligne_1.php
@@ -1,3 +1,4 @@
+<!-- Création des trois fichier pour les widgets de la sections ligne du footer -->
 <div id="sidebar-footer_ligne_1" class="sidebar">
     <?php dynamic_sidebar('footer_ligne_1'); ?>
 </div>
