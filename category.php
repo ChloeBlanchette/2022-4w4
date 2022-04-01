638f671299d1e72e597eea5c541f06b5f14579df
Chloé Blanchette <chloe.blanchette@outlook.com>
2022-04-01 17:15:33 -0400

    s9 c1 Chloé Blanchette Ajout d'un menu contenant des catégories de cours de manière à les filtrer

--- category.php
+++ category.php
@@ -1,10 +1,19 @@
 <?php get_header() ?>
 <main class="site__main">
+<?php if (is_category(array('cours', 'creation-3d', 'web', 'jeu', 'design', 'utilitaire', 'video'))): ?>
+    <?php wp_nav_menu(array(
+        "menu" => "categorie_cours",
+        "container" => "nav")); ?>
     <h2 class="site__main__titre">Liste des cours du programme TIM</h2>
     <?php if(have_posts()): ?>
         <section class="cours">
             <?php while(have_posts()): the_post(); ?>
-                <article class="cours__carte">
+            <?php 
+                $categories = get_the_category(); 
+                //var_dump($categories);
+                // echo $categories[1]->slug;
+            ?>
+                <article class="cours__carte <?php echo $categories[1]->slug; ?>">
                     <?php
                         $titre = get_the_title();
                         $titreCourt = substr(get_the_title(), 8);
@@ -27,5 +36,6 @@
             <?php endwhile; ?>
         </section>
     <?php endif; ?>
+    <?php endif; ?> <!-- if is category-->
 </main>
 <?php get_footer() ?>
