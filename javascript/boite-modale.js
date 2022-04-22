09c1ef28537c78f2324bbbc86a2daf2761469586
Chloé Blanchette <chloe.blanchette@outlook.com>
2022-04-22 19:13:14 -0400

    Ajout de la variable ciblant le bouton de fermeture de la boite modale

--- javascript/boite-modale.js
+++ javascript/boite-modale.js
@@ -2,7 +2,9 @@
     //console.log("vive la boite modale");
     let boite__modale = document.querySelector(".boite__modale");
     let cours__desc__ouvrir = document.querySelectorAll('.cours__desc__ouvrir');
-    // console.log(cours__desc__ouvrir.length);
+
+    // Ajout de la variable ciblant le bouton de fermeture de la boite modale
+    let boite__modale__fermeture = document.querySelectorAll(".boite__modale__fermeture");
 
     for (const bout of cours__desc__ouvrir) {
         
@@ -10,6 +12,15 @@
             // console.log(this.parentNode.parentNode.className);
             boite__modale.classList.add('ouvrir');
             console.log(boite__modale.classList);
+        })
+    }
+
+    for (const bouton of boite__modale__fermeture) {
+        
+        bouton.addEventListener('mousedown', function() {
+            // console.log(this.parentNode.parentNode.className);
+            boite__modale.classList.remove('ouvrir');
+            boite__modale.classList.add('fermer');
         })
     }
 
