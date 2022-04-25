99dc0752e9c60f2d2f7f5ac9c94caed3149f3c64
Chloé Blanchette <chloe.blanchette@outlook.com>
2022-04-25 08:59:06 -0400

    s12-c2 Chloé Blanchette Ajustement du sigle du cours et de la fonction qui permet la fermeture de la boite modale

--- gabarit/content-cours.php
+++ gabarit/content-cours.php
@@ -7,13 +7,13 @@
 <article class="cours__carte <?php echo $categories[1]->slug; ?>">
     <?php
         $titre = get_the_title();
-        $titreCourt = substr(get_the_title(), 8);
-        $titreCourt = substr($titreCourt, 0, strrpos($titreCourt, '('));
+        $titreCours = substr(get_the_title(), 8);
+        $titreCours = substr($titreCours, 0, strrpos($titreCours, '('));
 
         $nbHeures = get_field("nombre_dheures");
         $departement = get_field("departement");
 
-        $descCours = wp_trim_words(get_the_content(), 15, "<button class='cours__desc__ouvrir' href='#'> La suite </button>");
+        $desCours = get_the_content();
         /*  $posDebutHeures = strrpos($titre, '(') + 1;
             $posFinHeures = strrpos($titre, ')');
             $nbHeures = substr($titre, $posDebutHeures, $posFinHeures - $posDebutHeures);
@@ -22,11 +22,11 @@
     <?php the_post_thumbnail('thumbnail'); ?>
     <h2 class="carte__titre">
         <a href="<?php get_permalink(); ?>">
-            <?= $titreCourt; ?>
+            <?= $titreCours; ?>
         </a>
     </h2>
-    <p class="carte__code"><?= substr(get_the_title(), 0, 7) ?></p>
+    <p class="carte__code"><?= substr($titre, 4, 4); ?></p>
     <p><?= $nbHeures; ?></p>
-    <p class="carte__description"><?= $descCours ?></p>
+    <p class="carte__description"><?= wp_trim_words($desCours, 15, "<button class='cours__desc__ouvrir' href='#'> La suite </button>"); ?></p>
     <p class="cours__departement"><?= $departement; ?></p>
 </article>
--- javascript/boite-modale.js
+++ javascript/boite-modale.js
@@ -1,36 +1,23 @@
 (function() {
-    //console.log("vive la boite modale");
-    let boite__modale = document.querySelector(".boite__modale");
-    let cours__desc__ouvrir = document.querySelectorAll('.cours__desc__ouvrir');
 
-    // Ajout de la variable ciblant le bouton de fermeture de la boite modale
-    let boite__modale__fermeture = document.querySelectorAll(".boite__modale__fermeture");
+    let boite__modale = document.querySelector(".boite__modale");
+    let cours__desc__ouvrir = document.querySelectorAll(".cours__desc__ouvrir");
+    let boite__modale__fermeture = document.querySelector(".boite__modale__fermeture");
+    let boite__modale__texte = document.querySelector(".boite__modale__texte");
+
+    // Fonction qui permet la fermeture de la boite modale
+    boite__modale__fermeture.addEventListener('mousedown', function() {
+        boite__modale.classList.remove('ouvrir');
+    })
 
     // Ajout d'une fonction pour l'ouverture de la boite modale
     for (const bout of cours__desc__ouvrir) {
         
         // On ajoute un écouteur d'événement qui détecte le click de la souris sur le bouton La suite
         bout.addEventListener('mousedown', function() {
-            // console.log(this.parentNode.parentNode.className);
+
             // On ajoute la classe ouvrir à celle de la boite
             boite__modale.classList.add('ouvrir');
-            // On retire la classe fermer à celle de la boite
-            boite__modale.classList.remove('fermer');
-            console.log(boite__modale.classList);
-        })
-    }
-
-    // Ajout d'une fonction pour fermer la boite modale
-    for (const bouton of boite__modale__fermeture) {
-        
-        // On ajoute un écouteur d'événement qui détexte le click de la souris sur le bouton
-        bouton.addEventListener('mousedown', function() {
-            // console.log(this.parentNode.parentNode.className);
-
-            // On retire la classe ouvrir de la boite
-            boite__modale.classList.remove('ouvrir');
-            // On ajoute la classe fermer de la boite
-            boite__modale.classList.add('fermer');
         })
     }
 
