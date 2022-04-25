0c16480b96cf0d536288cdcdebd133dbca384565
Chloé Blanchette <chloe.blanchette@outlook.com>
2022-04-25 09:01:50 -0400

    s12-c2 Chloé Blanchette Retrait de la classe ouvrir de la boite modale lorsqu'on appuie sur le X pour la fermer

--- javascript/boite-modale.js
+++ javascript/boite-modale.js
@@ -5,11 +5,6 @@
     let boite__modale__fermeture = document.querySelector(".boite__modale__fermeture");
     let boite__modale__texte = document.querySelector(".boite__modale__texte");
 
-    // Fonction qui permet la fermeture de la boite modale
-    boite__modale__fermeture.addEventListener('mousedown', function() {
-        boite__modale.classList.remove('ouvrir');
-    })
-
     // Ajout d'une fonction pour l'ouverture de la boite modale
     for (const bout of cours__desc__ouvrir) {
         
@@ -20,5 +15,12 @@
             boite__modale.classList.add('ouvrir');
         })
     }
+
+    // Fermeture de la boite modale à l'aide d'un écouteur d'événement qui détecte le click de la souris sur le bouton de fermeture
+    boite__modale__fermeture.addEventListener('mousedown', function() {
+
+        // On retire la classe "ouvrir" de la boite modale lorsqu'on appuie sur le X pour la fermer
+        boite__modale.classList.remove('ouvrir');
+    })
 
 })()
